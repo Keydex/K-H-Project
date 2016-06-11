7cfb287bbd221b9c1af87be40290de808bdc25da
KeyDex <aphamx.mail+1@gmail.com>
2016-06-10 19:49:03 -0500

    Added Navbar Selection Coloring

--- source_js/script.js
+++ source_js/script.js
@@ -14,3 +14,38 @@ $(window).resize(function(){
 $('.mobile-menu a').click(function() {
   $('.mobile-menu').hide();
 });
+
+$('#contact').click(function() {
+	$('.menugray').css('color', '#969696');
+	$('#contact').css('color', '#8DAF4C');
+});
+
+$('#company').click(function() {
+	$('.menugray').css('color', '#969696');
+	$('#company').css('color', '#8DAF4C');
+});
+
+$('#services').click(function() {
+	$('.menugray').css('color', '#969696');
+	$('#services').css('color', '#8DAF4C');
+});
+
+$('#portfolio').click(function() {
+	$('.menugray').css('color', '#969696');
+	$('#portfolio').css('color', '#8DAF4C');
+});
+
+$('#plan-by-us').click(function() {
+	$('.menugray').css('color', '#969696');
+	$('#plan-by-us').css('color', '#8DAF4C');
+});
+
+$('#plan-by-others').click(function() {
+	$('.menugray').css('color', '#969696');
+	$('#plan-by-others').css('color', '#8DAF4C');
+});
+
+$('#logoClick').click(function() {
+	console.log("Clicked Logo");
+	$('.menugray').css('color', '#969696');
+});
